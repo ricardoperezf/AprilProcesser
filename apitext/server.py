8a536b5d6f462ccee30161a13c18e65827ab7f4b
Ricardo Pérez <ricardojpf98@gmail.com>
2017-10-23 01:20:35 -0600

    RP
    Added new comments to the new method called finder.
    
    I added vector append to resto de palabra's count.
    
    Thanks God.

--- apitext/server.py
+++ apitext/server.py
@@ -8,6 +8,7 @@ count_verbs = 0
 count_nouns = 0
 count_adjective = 0
 count_pronouns = 0
+count_another_word = 0
 count_total_words = 0
 
 
@@ -22,6 +23,7 @@ def get_text_file():
     global count_nouns
     global count_adjective
     global count_pronouns
+    global count_another_word
     global count_total_words
 
     if request.method == "POST":
@@ -34,13 +36,14 @@ def get_text_file():
         vector.append("Sustantivos: " + str(count_nouns))
         vector.append("Adjetivos: " + str(count_adjective))
         vector.append("Pronombres: " + str(count_pronouns))
+        vector.append("Resto: " + str(count_another_word))
         vector.append("Total palabras: " + str(count_total_words))
         return jsonify(vector)
 
 
 ########################################################################################################################
 #   MÉTODO QUE PERMITE HACER SPLIT A CADA LINEA QUE LLEGA DE UN CLIENTE Y SEPARARLA EN PALABRAS.
-#   LUEGO PERMITRÁ HACER LLAMADOS A LOS MÉTODOS DE BUSCAR VERBO, SUSTANTIVO, ADJETIVO, PRONOMBRE.
+#   LUEGO PERMITRÁ HACER LLAMADOS AL MÉTODO QUE HACE PIVOT A LOS MÉTODOS DE PALABRAS.
 
 def split_sentences(output):
     global count_total_words
@@ -48,21 +51,24 @@ def split_sentences(output):
         count_total_words += 1
         if "," in word:
             new_word = word.replace(',', '')
-            find_verb(new_word)
-            find_noun(new_word)
-            find_adjective(new_word)
-            find_pronoun(new_word)
+            finder(new_word)
         elif "." in word:
             new_word = word.replace(".", "")
-            find_verb(new_word)
-            find_noun(new_word)
-            find_adjective(new_word)
-            find_pronoun(new_word)
+            finder(new_word)
         else:
-            find_verb(word)
-            find_noun(word)
-            find_adjective(word)
-            find_pronoun(word)
+            finder(word)
+
+
+# MÉTODO QUE PERMITE HACER LOS DISTINTOS LLAMADOS DE CADA TIPO DE PALABRA PARA HACER EL ANÁLISIS COMPARATIVO
+#   DE LOS MÉTODOS DE BUSCAR VERBO, SUSTANTIVO, ADJETIVO, PRONOMBRE.
+def finder(word):
+    global count_another_word
+    finder_of_verb = find_verb(word)
+    finder_of_noun = find_noun(word)
+    finder_of_adjective = find_adjective(word)
+    finder_of_pronoun = find_pronoun(word)
+    if finder_of_verb != "Se encontro" and finder_of_noun != "Se encontro" and finder_of_adjective != "Se encontro" and finder_of_pronoun != "Se encontro":
+        count_another_word += 1
 
 
 # MÉTODO QUE PERMITE HACER EL ANÁLISIS COMPARATIVO ENTRE LAS PALABRAS RECIBIDAD DEL TXT FILE CON LAS PALABRAS DEL TXT
@@ -78,7 +84,7 @@ def find_verb(palabra):
                     # print("\n\nSE ENCONTRO EL VERBO = " + word + " Y LA PALABRA FUE = " + palabra + "\n\n" + " COUNT = " + str(count_verbs))
                     # new_index = "Se encontro = " + palabra + " count = " + str(count_verbs)
                     # vector_verb.append(new_index)
-                    return "Se encontró un verbo"
+                    return "Se encontro"
 
 
 # MÉTODO QUE PERMITE HACER EL ANÁLISIS COMPARATIVO ENTRE LAS PALABRAS RECIBIDAD DEL TXT FILE CON LAS PALABRAS DEL TXT
@@ -94,7 +100,7 @@ def find_noun(palabra):
                     # print("\n\nSE ENCONTRO EL SUSTANTIVO = " + word + " Y LA PALABRA FUE = " + palabra + "\n\n" + " COUNT = " + str(count_nouns))
                     # new_index = "Se encontro el sustantivo = " + palabra + " count = " + str(count_nouns)
                     # vector_verb.append(new_index)
-                    return "Se encontró un sustantivo"
+                    return "Se encontro"
 
 
 # MÉTODO QUE PERMITE HACER EL ANÁLISIS COMPARATIVO ENTRE LAS PALABRAS RECIBIDAD DEL TXT FILE CON LAS PALABRAS DEL TXT
@@ -110,7 +116,7 @@ def find_adjective(palabra):
                     # print("\n\nSE ENCONTRO EL ADJETIVO = " + word + " Y LA PALABRA FUE = " + palabra + "\n\n" + " COUNT = " + str(count_adjective))
                     # new_index = "Se encontro = " + palabra + " count = " + str(count_adjective)
                     # vector.append(new_index)
-                    return "Se encontró un adjetivo"
+                    return "Se encontro"
 
 
 # MÉTODO QUE PERMITE HACER EL ANÁLISIS COMPARATIVO ENTRE LAS PALABRAS RECIBIDAD DEL TXT FILE CON LAS PALABRAS DEL TXT
@@ -126,7 +132,7 @@ def find_pronoun(palabra):
                     # print("\n\nSE ENCONTRO EL PRONOMBRE = " + word + " Y LA PALABRA FUE = " + palabra + "\n\n" + " COUNT = " + str(count_pronouns))
                     # new_index = "Se encontro = " + palabra + " count = " + str(count_verbs)
                     # vector.append(new_index)
-                    return "Se encontró un pronombre"
+                    return "Se encontro"
 
 
 ########################################################################################################################
