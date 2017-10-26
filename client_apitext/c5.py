c1ba9bd925975c2829b80eed267a167ce2f61874
Ricardo Pérez <ricardojpf98@gmail.com>
2017-10-26 14:38:18 -0600

    RP
    Changed the client to a better client with UI.

---
+++ client_apitext/c5.py
@@ -0,0 +1,7 @@
+
+def ejemplo():
+    ejemplo = "ricardo;"
+    word = ejemplo.replace(";", "")
+    print("VIEJA PALABRA: " + ejemplo)
+    print("NUEVA PALABRA: " + word)
+ejemplo()
--- client_apitext/client.py
+++ client_apitext/client.py
@@ -1,12 +1,30 @@
+from flask import Flask, render_template
 import requests
-import time
+import json
 
-def index(word):
-     post_request = requests.post('http://192.168.0.15:5000/api/v1?ejemplo=' + str(word))
-     print(post_request.text)
+########################################################################################################################
 
-def splitter():
-    with open('Ortega Y Gasset, José - El Sentido Histórico De La Teoría De Einstein.txt', 'r') as f:
-        for line in f:
-            index(line)
-splitter()
+app = Flask(__name__)
+
+
+########################################################################################################################
+
+#   MÉTODO QUE PERMITE MEDIANTE LA RUTA DE /START HACER EL POST AL SERVIDOR
+@app.route('/start')
+def post_server():
+    libro = open('Ortega Y Gasset, José - El Sentido Histórico De La Teoría De Einstein.txt', 'r')
+    word = libro.read()
+    post_request = requests.post('http://192.168.0.15:5000/api/v1', data={"ejemplo": str(word)})
+    return render_template('./pingpongUI.html', resultado=json.dumps(post_request.text))
+
+
+#   MÉTODO QUE PERMITE MEDIANTE LA RUTA DE /RESULTADO VER EL PROCESAMIENTO DEL TXT FILE.
+@app.route('/resultado')
+def get_final_result():
+    theRequest = requests.get('http://192.168.0.15:5000/api/v1')
+    return render_template('./pingpongUI.html', resultado=json.dumps(theRequest.text))
+
+
+########################################################################################################################
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
