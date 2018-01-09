4df82fdd9f3257009476e44acde9b0267cf45f22
ricardoperezf <ricardojpf98@gmail.com>
2018-01-09 00:42:29 -0600

    Modified search

--- aprilprocesser/models/search.py
+++ aprilprocesser/models/search.py
@@ -1,3 +1,5 @@
+from .books import *
+
 class Search:
     def __init__(self, text_file):
         self.text_file = text_file
@@ -16,6 +18,11 @@ class Search:
     def split_sentences(self):
         output = self.text_file
         for word in output.split():
+            print("\n\n")
+            import os
+            os.getcwd()
+            print(os.path.exists("books/verbs/verbos_klmno.txt"))
+            print("\n\n")
             self.count_total_words += 1
             if "," in word:
                 new_word = word.replace(',', '')
@@ -60,32 +67,32 @@ class Search:
 
     def find_first_letter(self, the_word):
         if the_word[0] == "a" or the_word[0] == "b" or the_word[0] == "c" or the_word[0] == "d" or the_word[0] == "e":
-            self.verb_text_file = "books/verbos/verbos_abcde.txt"
+            self.verb_text_file = "books/verbs/verbos_abcde.txt"
             self.noun_text_file = "books/sustantivos/sustantivos_abcde.txt"
             self.adjective_text_file = "books/adjetivos/adjetivos_abcde.txt"
             self.pronoun_text_file = "books/pronombres/pronombres_abcde.txt"
             return self.finder_brain(the_word)
         elif the_word[0] == "f" or the_word[0] == "g" or the_word[0] == "h" or the_word[0] == "i" or the_word[0] == "j":
-            self.verb_text_file = "books/verbos/verbos_fghij.txt"
+            self.verb_text_file = "books/verbs/verbos_fghij.txt"
             self.noun_text_file = "books/sustantivos/sustantivos_fghij.txt"
             self.adjective_text_file = "books/adjetivos/adjetivos_fghij.txt"
             self.pronoun_text_file = "books/pronombres/pronombres_fghij.txt"
             return self.finder_brain(the_word)
         elif the_word[0] == "k" or the_word[0] == "l" or the_word[0] == "m" or the_word[0] == "n" or the_word[0] == "o":
-            self.verb_text_file = "books/verbos/verbos_klmno.txt"
+            self.verb_text_file = "books/verbs/verbos_klmno.txt"
             self.noun_text_file = "books/sustantivos/sustantivos_klmno.txt"
             self.adjective_text_file = "books/adjetivos/adjetivos_klmno.txt"
             self.pronoun_text_file = "books/pronombres/pronombres_klmno.txt"
             return self.finder_brain(the_word)
         elif the_word[0] == "p" or the_word[0] == "q" or the_word[0] == "r" or the_word[0] == "s" or the_word[0] == "t":
-            self.verb_text_file = "books/verbos/verbos_pqrst.txt"
+            self.verb_text_file = "books/verbs/verbos_pqrst.txt"
             self.noun_text_file = "books/sustantivos/sustantivos_pqrst.txt"
             self.adjective_text_file = "books/adjetivos/adjetivos_pqrst.txt"
             self.pronoun_text_file = "books/pronombres/pronombres_pqrst.txt"
             return self.finder_brain(the_word)
         elif the_word[0] == "u" or the_word[0] == "v" or the_word[0] == "w" or the_word[0] == "x" or the_word[
             0] == "y" or the_word[0] == "z":
-            self.verb_text_file = "books/verbos/verbos_uvwxyz.txt"
+            self.verb_text_file = "books/verbs/verbos_uvwxyz.txt"
             self.noun_text_file = "books/sustantivos/sustantivos_uvwxyz.txt"
             self.adjective_text_file = "books/adjetivos/adjetivos_uvwxyz.txt"
             self.pronoun_text_file = "books/pronombres/pronombres_uwxyz.txt"
@@ -96,7 +103,7 @@ class Search:
         finder_of_noun = self.find_noun(word)
         finder_of_adjective = self.find_adjective(word)
         finder_of_pronoun = self.find_pronoun(word)
-        if finder_of_verb != "Se encontro" and finder_of_noun != "Se encontro" and finder_of_adjective != "Se encontro" and finder_of_pronoun != "Se encontro":
+        if finder_of_verb != "It founded" and finder_of_noun != "It founded" and finder_of_adjective != "It founded" and finder_of_pronoun != "It founded":
             self.count_another_word += 1
         return self.count_another_word
 
@@ -130,7 +137,7 @@ class Search:
                 for word in line.split():
                     if word_parameter == word.lower():
                         self.count_pronouns += 1
-                        return "Se encontro"
+                        return "It founded"
 
     def get_total_count(self):
         self.vector.append({'verbs': self.count_verbs})
