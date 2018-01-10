f224fcbe42a036d282df83f46e7674ad3a6f4128
ricardoperezf <ricardojpf98@gmail.com>
2018-01-09 18:23:53 -0600

    Ready search's post

--- aprilprocesser/endpoints/search_controller.py
+++ aprilprocesser/endpoints/search_controller.py
@@ -1,15 +1,25 @@
-from aprilprocesser import aprilprocesser_app
+from aprilprocesser import aprilprocesser_app, mongo
 from flask import request, jsonify
 from ..models.search import Search
 
 output = Search
 
+
 @aprilprocesser_app.route('/api/v1/text', methods=['POST'])
 def post_text_file():
     global output
     output = request.form['ejemplo']
-    the_search = Search(output).split_sentences()
-    return jsonify(the_search), 201
+    get_search = Search(output).split_sentences()
+    save_search = mongo.db.searches
+    print("\n")
+    print(get_search)
+    print(get_search[-1])
+    print("\n")
+    save_search.insert(
+        {'verbs': get_search[-1]['verbs'], 'nouns': get_search[-1]['nouns'], 'adjectives': get_search[-1]['adjectives'],
+         'pronouns': get_search[-1]['pronouns'], 'other_words': get_search[-1]['other_words'],
+         'total_words': get_search[-1]['total_words']})
+    return jsonify(get_search), 201
 
 
 @aprilprocesser_app.route('/api/v1/text', methods=['GET'])
